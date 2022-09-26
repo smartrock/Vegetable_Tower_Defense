d185a584a4929934ea5a3fe6fcdc95029d480609
smartrock <samorsler@mmc.school.nz>
2022-09-27 12:29:13 +1300

    Waves changes

--- Waves.cs
+++ Waves.cs
@@ -10,7 +10,7 @@ namespace Vegetable_Tower_Defense
 {
     class Waves
     {
-        public int x, y, width, height, speed;
+        public int x, y, movespeed;
         Point Turn1 = new Point(130, 145);
         Point Turn2 = new Point(130, 355);
         Point Turn3 = new Point(275, 145);
@@ -26,41 +26,40 @@ namespace Vegetable_Tower_Defense
             //yw = vegearea.Y + vegearea.Width / 2;
         }
 
-        public void DrawWaves(Graphics g)
-        {
-            //vegecentre = new Point(x, y);
-            // Drawing the images and there respective rectangles
-        }
+        //public void DrawWaves(Graphics g)
+        //{
+            
+        //}
 
         public void MoveWaves(Graphics g)
         {
             if (x < Turn1.X)
             {
-                x += speed;
+                x += movespeed;
             }
             else if (x == Turn1.X && y != Turn2.Y)
             {
-                y += speed;
+                y += movespeed;
             }
             else if (y == Turn2.Y && x != Turn3.X)
             {
-                x += speed;
+                x += movespeed;
             }
             else if (x == Turn3.X && y != Turn4.Y)
             {
-                y -= speed;
+                y -= movespeed;
             }
             else if (y == Turn4.Y && x != Turn5.X)
             {
-                x += speed;
+                x += movespeed;
             }
             else if (x == Turn5.X && y != Turn6.Y)
             {
-                y += speed;
+                y += movespeed;
             }
             else if (y == Turn6.Y && x < 580)
             {
-                x += speed;
+                x += movespeed;
             }
         }
 
