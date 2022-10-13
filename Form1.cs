97966a6b1f673e8e1c1564e718c8b7a5c2082791
smartrock <107009802+smartrock@users.noreply.github.com>
2022-10-13 13:02:01 +1300

    Begining unit to mouse movement

--- Form1.cs
+++ Form1.cs
@@ -25,6 +25,7 @@ namespace Vegetable_Tower_Defense
         public FrmGame()
         {
             InitializeComponent();
+            // Stops panel on game screen flickering when it is redrawn in quick sucession
             typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, PnlGame, new object[] { true });
             TmrMissile.Enabled = false;
             TmrWave.Enabled = false;
@@ -46,62 +47,68 @@ namespace Vegetable_Tower_Defense
             //    m.moveMissile(g);
             //}
 
-            foreach (Units u in unit)
+            foreach (Units u in unit) // The drawing and movement controls for the units class
             {
                 u.DrawUnit(g);
+                u.MoveUnit(g);
             }
-            foreach (Vegetables v in GlobalVariables.vegetables)
+            foreach (Vegetables v in GlobalVariables.vegetables) // The drawing and movement controls for the vegetables 
             {
                 v.MoveVegetables(g);
                 v.DrawVegetables(g);
             }
+            // Updating values on game screen
+            LblScore.Text = score.ToString();
+            LblLives.Text = lives.ToString();
+            LblWaves.Text = waves.ToString();
+            LblMoney.Text = money.ToString();
         }
 
         public void PreLevel()
         {
-            int b;
-            b = waves;
-            for (int i = 0; i < b; i++)
+            int n;
+            n = waves;
+            for (int i = 0; i < n; i++)
             {  
                 // Adds same number of basic level vegetables as wave count
                 GlobalVariables.vegetables.Add(new Vegetables(0));
             }
-            if (b >= (b - 5))
+            if (n >= (n - 5))
             {
                 // Adds 5 less of second level vegetables as number in wave count
-                for (int i = 0; i < (b - 5); i++)
+                for (int i = 0; i < (n - 5); i++)
                 {
                     GlobalVariables.vegetables.Add(new Vegetables(1));
                 }
             }
-            if (b >= (b - 10))
+            if (n >= (n - 10))
             {
                 // Adds 10 less of second level vegetables as number in wave count
-                for (int i = 0; i < (b - 10); i++)
+                for (int i = 0; i < (n - 10); i++)
                 {
                     GlobalVariables.vegetables.Add(new Vegetables(2));
                 }
             }
-            if (b >= (b - 15))
+            if (n >= (n - 15))
             {
                 // Adds 15 less of second level vegetables as number in wave count
-                for (int i = 0; i < (b - 15); i++)
+                for (int i = 0; i < (n - 15); i++)
                 {
                     GlobalVariables.vegetables.Add(new Vegetables(3));
                 }
             }
-            if (b >= (b - 20))
+            if (n >= (n - 20))
             {
                 // Adds 20 less of second level vegetables as number in wave count
-                for (int i = 0; i < (b - 20); i++)
+                for (int i = 0; i < (n - 20); i++)
                 {
                     GlobalVariables.vegetables.Add(new Vegetables(4));
                 }
             }
-            if (b >= (b - 25))
+            if (n >= (n - 25))
             {
                 // Adds 25 less of second level vegetables as number in wave count
-                for (int i = 0; i < (b - 25); i++)
+                for (int i = 0; i < (n - 25); i++)
                 {
                     GlobalVariables.vegetables.Add(new Vegetables(5));
                 }
@@ -113,6 +120,9 @@ namespace Vegetable_Tower_Defense
         {
             TmrWave.Enabled = false; // Stops waves timer
             TmrMissile.Enabled = false; // Stops units missile timer
+            MnuPlay.Enabled = true;
+            MnuPause.Enabled = false;
+            
         }
 
         private void MnuPlay_Click(object sender, EventArgs e)
@@ -123,6 +133,7 @@ namespace Vegetable_Tower_Defense
             TmrMissile.Enabled = true; // Starts units missile timer
             TmrScreen.Enabled = true; // Checks screen timer is running
             PreLevel(); // Calls function to run level
+            MnuPause.Enabled = true;
             MnuPlay.Enabled = false; // Disables play button while level / waves is in action
         }
 
@@ -157,7 +168,11 @@ namespace Vegetable_Tower_Defense
             GlobalVariables.vegieInfo.Add(new GetVegieInfo(4, 30, 1));
 
             //Units specific info
+            GlobalVariables.unitInfo.Add(new GetUnitInfo(0, 1, 1, 1));
             GlobalVariables.unitInfo.Add(new GetUnitInfo(1, 1, 1, 1));
+            GlobalVariables.unitInfo.Add(new GetUnitInfo(3, 1, 1, 1));
+            GlobalVariables.unitInfo.Add(new GetUnitInfo(4, 1, 1, 1));
+            GlobalVariables.unitInfo.Add(new GetUnitInfo(5, 1, 1, 1));
         }
         private void TxtName_KeyPress_1(object sender, KeyPressEventArgs e)
         {
@@ -167,17 +182,34 @@ namespace Vegetable_Tower_Defense
                 // If key pressed not letter then don't add it to text box
                 e.Handled = true;
             }
-
-            if (TxtName.Text != "") // If the text box isn't empty then enable start button
-            {
-                MnuPlay.Enabled = true;
-                MnuPause.Enabled = true;
-            }
+            
             if (TxtName.Text == "") // If the text box isn't empty then enable start button
             {
                 MnuPlay.Enabled = false;
                 MnuPause.Enabled = false;
             }
+            
+            if (TxtName.Text != "") // If the text box isn't empty then enable start button
+            {
+                MnuPlay.Enabled = true;
+                MnuPause.Enabled = true;
+            }
         }
+        
+        //private void Form1_MouseDown(object sender, MouseEventArgs e)
+        //{
+        //    foreach (Rectangle r in units)
+        //    {
+        //        if (area[count].Contains(e.X, e.Y))
+        //        {
+        //            area[count].Location = new Point(e.X, e.Y);
+        //        }
+        //    }
+        //}
+        
+        //private void Form1_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    units.MoveUnit(e.X, e.Y);
+        //}
     }
 }
