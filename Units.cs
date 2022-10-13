1484b81c86a0b842bb35b53cd73fb517bd0ebe8c
smartrock <107009802+smartrock@users.noreply.github.com>
2022-10-13 13:31:31 +1300

    Update Units.cs

--- Units.cs
+++ Units.cs
@@ -10,10 +10,9 @@ namespace Vegetable_Tower_Defense
 {
     class Units
     {
-        public int x, y, width, height, type, speed, range, damage;
-        public string unittype;
-        public Rectangle unitrec = new Rectangle();
-        public Image[] unitimage = new Image[5];
+        public int x, y, width, height, type, speed, range, damage, cost; // Declearing in use variables 
+        public Rectangle unitrec = new Rectangle(); // rectangle for unit image to be inside
+        public Image[] unitimage = new Image[5]; // Image array for the different unit types
 
         public Units(int X, int Y, int Type)
         {
@@ -21,10 +20,12 @@ namespace Vegetable_Tower_Defense
             {
                 if (i.Type == Type)
                 {
+                    // Gets the specific unit info from global variables
                     health = i.Health;
                     speed = i.Speed;
                     type = i.Type;
                     damage = i.Damage;
+                    cost = i.Cost;
                 }
             }
 
@@ -33,38 +34,26 @@ namespace Vegetable_Tower_Defense
             width = 50;
             height = 50;
 
-            if (unittype == "1")
-            {
-                unitimage = Properties.Resources.holding;
-            }
-            if (unittype == "2")
-            {
-                unitimage = Properties.Resources.holding2;
-            }
-            if (unittype == "3")
-            {
-                unitimage = Properties.Resources.holding3;
-            }
-            if (unittype == "4")
-            {
-                unitimage = Properties.Resources.holding4;
-            }
-            if (unittype == "5")
-            {
-                unitimage = Properties.Resources.holding5;
-            }
+            // Sets up the correct image with the corresponding type number
+            unitimage[0] = Properties.Resources.holding;
+            unitimage[1] = Properties.Resources.holding1;
+            unitimage[2] = Properties.Resources.holding2;
+            unitimage[3] = Properties.Resources.holding3;
+            unitimage[4] = Properties.Resources.holding4;
+            // Creates rectangle for units
             unitrec = new Rectangle(x, y, width, height);
         }
 
         public void DrawUnit(Graphics g)
         {
-            unitrec = new Rectangle(x, y, width, height);
-            g.DrawImage(unitimage, unitrec);
+            unitrec = new Rectangle(x, y, width, height); // Binds location and size together
+            g.DrawImage(unitimage[type], unitrec); // Draws image with its rectangle
         }
         
         public void MoveUnit(Graphics g)
         {
-            unitrec.Location = new Point(x,y);
+            
+            unitrec.Location = new Point(x,y); // Sets new rectangle location
         }
     }
 }
