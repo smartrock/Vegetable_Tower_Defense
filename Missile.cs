c12456d6a928d9abbc769bcdeba0d09bc3f66fce
smartrock <107009802+smartrock@users.noreply.github.com>
2022-10-20 12:45:09 +1300

    Changing to radians

--- Missile.cs
+++ Missile.cs
@@ -21,14 +21,14 @@ namespace Vegetable_Tower_Defense
         Point centreMissile;
 
         //Create a constructor (initialises the values of the fields)
-        public Missile(Rectangle unitrec)
+        public Missile(Rectangle unitrec, int CurrentMouseX, int CurrentMouseY)
         {
             width = 20; // Width and height of missile rectange
             height = 20;
             x = unitrec.X + (width / 2); // Starting position at unit location
             y = unitrec.Y + (height / 2); // Starting position at unit location
             missileImage = Properties.Resources.missile; //planetImage contains the plane1.png image
-            missileRotate = Math.Atan((GlobalVariables.MouseXPos - x) / (GlobalVariables.MouseYPos - y));
+            missileRotate = Math.Atan(((CurrentMouseX - x) / (CurrentMouse - y) * Math.PI / 180)); // Calculating angle for missiles
             missileRotated = (int)missileRotate; // Casting double to a int
             //this code works out the speed of the missile to be used in the moveMissile method
             xSpeed = 30 * (Math.Cos((missileRotated - 90) * Math.PI / 180));
